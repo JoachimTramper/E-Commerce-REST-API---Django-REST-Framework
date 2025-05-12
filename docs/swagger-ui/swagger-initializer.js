11c87e2d2e5dfb86f7056718a9c3522d908120b4
Joachim Tramper <joachimtramper@gmail.com>
2025-05-12 16:11:33 +0700

    chore: configure Swagger-UI to load local schema.yml with Try it out

---
+++ docs/swagger-ui/swagger-initializer.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+  const ui = SwaggerUIBundle({
+    url: "../schema.yml",
+    dom_id: '#swagger-ui',
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    layout: "BaseLayout",
+    tryItOutEnabled: true,
+  });
+  window.ui = ui;
+};
